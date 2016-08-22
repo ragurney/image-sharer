d01181de4c1cabf65a05229b40bac90c69ed9417
Ryan Gurney <ryan.a.gurney@gmail.com>
2016-08-22 14:01:15 -0700

    rg/hz - add controller method create

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,21 @@ class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:success] = 'Url successfully saved!'
+      redirect_to image_path(id: @image.id)
+    else
+      flash[:error] = 'Url cannot be empty'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,10 +1,29 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @url = 'http://jstsolutions.net/wp-content/themes/realty/lib/images/key_img2.png'
+    @image = Image.create(url: @url)
+  end
+
   test 'should get images form page' do
     get new_image_path
 
     assert_response :ok
     assert_select 'h1', 1, 'New Image URL'
+  end
+
+  test 'controller should create and put correctly' do
+    post images_path(image: { url: @url })
+
+    assert_response :found
+    assert_not_nil Image.find_by(url: @url)
+  end
+
+  test 'invalid url does not create image' do
+    post images_path(image: { url: nil })
+
+    assert_response :unprocessable_entity
+    assert_equal 'Url cannot be empty', flash[:error]
   end
 end
