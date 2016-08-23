58e83dd4a2e25ee3f2e2492223ab68e8b5cb0d5a
Ryan Gurney <ryan.a.gurney@gmail.com>
2016-08-23 11:33:07 -0700

    Implement url validation in image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,15 @@
+require 'uri'
+
 class Image < ActiveRecord::Base
   validates :url, presence: true
+  validate :url_is_valid
+
+  private
+
+  def url_is_valid
+    return unless url.present?
+
+    errors.add(:url, 'must be image') unless
+      (File.extname(url) =~ /^(.png|.gif|.jpg)$/) && (url =~ /^#{URI.regexp}$/)
+  end
 end
