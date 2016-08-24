b726cde127181da3fd794ebafd17f37bac3fac2b
Ryan Gurney <ryan.a.gurney@gmail.com>
2016-08-24 12:52:45 -0700

    Implement seeds for Image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+url_list = %w(https://static.pexels.com/photos/1518/black-and-white-city-bird-people.jpg
+              http://digital-photography-school.com/wp-content/uploads/2007/02/black-and-white-tips.jpg
+              https://s-media-cache-ak0.pinimg.com/736x/56/5a/9d/565a9d198452ee8884db9240b63b9ee4.jpg
+              http://inspirationfeeed.files.wordpress.com/2014/03/20481.jpg
+              https://www.art-competition.net/images/winners/Black_White/H7_Laurent_Baheux_Lion-portrait-Kenya.jpg)
+
+4.times do
+  url_list.each do |url|
+    Image.create!(url: url)
+  end
+end
