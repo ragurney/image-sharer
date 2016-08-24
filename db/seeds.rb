7bfd034878a734a150870e35d500967d6e4e2806
Ryan Gurney <ryan.a.gurney@gmail.com>
2016-08-24 15:19:48 -0700

    Use one call to create! and delete stock comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 url_list = %w(https://static.pexels.com/photos/1518/black-and-white-city-bird-people.jpg
               http://digital-photography-school.com/wp-content/uploads/2007/02/black-and-white-tips.jpg
               https://s-media-cache-ak0.pinimg.com/736x/56/5a/9d/565a9d198452ee8884db9240b63b9ee4.jpg
@@ -13,7 +5,11 @@ url_list = %w(https://static.pexels.com/photos/1518/black-and-white-city-bird-pe
               https://www.art-competition.net/images/winners/Black_White/H7_Laurent_Baheux_Lion-portrait-Kenya.jpg)
 
 4.times do
-  url_list.each do |url|
-    Image.create!(url: url)
-  end
+  Image.create!([
+    { url: url_list[0] },
+    { url: url_list[1] },
+    { url: url_list[2] },
+    { url: url_list[3] },
+    { url: url_list[4] }
+  ])
 end
