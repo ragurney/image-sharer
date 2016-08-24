ad3287eb1441561af890b88e633cdeba2d62b376
Ryan Gurney <ryan.a.gurney@gmail.com>
2016-08-24 12:35:37 -0700

    Removed port support from regex

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ require 'uri'
 
 class Image < ActiveRecord::Base
   URL_REGEX = %r{ \A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)
-                  *\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?(\/)?(.png|.jpg|.gif)\z }x
+                  (.*)?(\/)?(.png|.jpg|.gif)\z }x
 
   validates :url, presence: true
   validates :url, format: { with: URL_REGEX, message: 'must be a valid url', if: :url? }
