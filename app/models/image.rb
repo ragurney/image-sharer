1069ef1367c06c0fc8c516d531bc35e0b6420ff1
Ryan Gurney <ryan.a.gurney@gmail.com>
2016-08-26 17:37:44 -0700

    Refactored regex to use gem instead

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,5 @@
 require 'uri'
 
 class Image < ActiveRecord::Base
-  URL_REGEX = %r{ \A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)
-                  (.*)?(\/)?(.png|.jpg|.gif)\z }x
-
-  validates :url, presence: true
-  validates :url, format: { with: URL_REGEX, message: 'must be a valid url', if: :url? }
+  validates :url, presence: true, url: { allow_blank: true, message: 'must be a valid url' }
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -2,30 +2,13 @@ require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
   test 'check url valid' do
-    valid_urls.each do |url|
-      extensions.each do |ext|
-        img = Image.new(url: "#{url}#{ext}")
-        assert img.valid?
-      end
-    end
+    image = Image.new(url: 'https://thisis.com/?stuff.png')
+    assert_predicate image, :valid?
   end
 
   test 'check url invalid' do
-    invalid_urls.each do |url|
-      extensions.each do |ext|
-        img = Image.new(url: "#{url}#{ext}")
-        refute img.valid?
-        assert_equal ['must be a valid url'], img.errors.messages[:url]
-      end
-    end
-  end
-
-  test 'url invalid with no extension' do
-    valid_urls.each do |url|
-      img = Image.new(url: url)
-      refute img.valid?
-      assert_equal ['must be a valid url'], img.errors.messages[:url]
-    end
+    image = Image.new(url: 'thisis.com/?invalid.png')
+    assert_predicate image, :invalid?
   end
 
   test 'url invalid with empty string' do
@@ -38,19 +21,5 @@ class ImageTest < ActiveSupport::TestCase
     img = Image.new
     refute img.valid?
     assert_equal ["can't be blank"], img.errors.messages[:url]
-  end
-
-  private
-
-  def valid_urls
-    ['http://thisis.com/?valid_yay', 'http://thisis.com/?valid_yay']
-  end
-
-  def invalid_urls
-    ['thisis.com/?invalid', 'asdf https://thisis.com?invalid']
-  end
-
-  def extensions
-    ['.jpg', '.png', '.gif']
   end
 end
